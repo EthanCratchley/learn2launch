68107863fe373df183e36d90974638f2530c9cfe
Ethan Cratchley <138141942+EthanCratchley@users.noreply.github.com>
2024-01-19 12:55:52 -0800

    homepage progress

--- frontend/js/script.js
+++
@@ -1,9 +0,0 @@
-function scrollLeft() {
-    const galleryWrapper = document.querySelector('.gallery-wrapper');
-    galleryWrapper.scrollBy({ left: -200, behavior: 'smooth' }); // Adjust -200 if needed
-}
-
-function scrollRight() {
-    const galleryWrapper = document.querySelector('.gallery-wrapper');
-    galleryWrapper.scrollBy({ left: 200, behavior: 'smooth' }); // Adjust 200 if needed
-}
